5f152e76665833a618232d806c8c9947edd645ac
David Moranchel <david.arcshtag@gmail.com>
2021-03-17 21:34:54 -0600

    List and Detail view examples

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from .views import Owners, Test
+from .views import Owners, Test, OwnersList, OwnersDetail
 
-urlpatterns = [path("owners/", Owners.as_view()), path("test/", Test.as_view())]
+urlpatterns = [
+    path("owners/", OwnersList.as_view()),
+    path("owners/<int:pk>/", OwnersDetail.as_view()),
+    path("test/", Test.as_view()),
+]
--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.views.generic import View
+from django.views.generic import View, TemplateView, ListView, DetailView
 
 from .models import PetOwner
 
@@ -13,6 +13,29 @@ class Owners(View):
 
         template = loader.get_template("vet/owners/list.html")
         return HttpResponse(template.render(context, request))
+
+
+# class OwnersList(TemplateView):
+#     template_name = "vet/owners/list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         print(context, "ESTO VIENE DEL PADRE (TEMPLATEVIEW)")
+#         context["owners"] = PetOwner.objects.all()
+#         print(context, "ESTO LE AGREGAMOS NOSOTROS")
+#         return context
+
+
+class OwnersList(ListView):
+    model = PetOwner
+    template_name = "vet/owners/list.html"
+    context_object_name = "owners"
+
+
+class OwnersDetail(DetailView):
+    model = PetOwner
+    template_name = "vet/owners/detail.html"
+    context_object_name = "owner"
 
 
 class Test(View):
