8447cd5a2599f28f4397d7510a2e8c37be803817
David Moranchel <david.arcshtag@gmail.com>
2021-03-18 20:52:01 -0600

    pets flow

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import OwnersList, OwnersDetail
+from .views import OwnersList, OwnersDetail, PetsList, PetsDetail
 
 urlpatterns = [
     path("owners/", OwnersList.as_view(), name="owners_list"),
     path("owners/<int:pk>/", OwnersDetail.as_view(), name="owners_detail"),
+    path("pets/", PetsList.as_view(), name="pets_list"),
+    path("pets/<int:pk>/", PetsDetail.as_view(), name="pets_detail"),
 ]
--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import View, TemplateView, ListView, DetailView
 
-from .models import PetOwner
+from .models import PetOwner, Pet
 
 # Create your views here.
 class OwnersList(ListView):
@@ -16,3 +16,15 @@ class OwnersDetail(DetailView):
     model = PetOwner
     template_name = "vet/owners/detail.html"
     context_object_name = "owner"
+
+
+class PetsList(ListView):
+    model = Pet
+    template_name = "vet/pets/list.html"
+    context_object_name = "pets"
+
+
+class PetsDetail(DetailView):
+    model = Pet
+    template_name = "vet/pets/detail.html"
+    context_object_name = "pet"
